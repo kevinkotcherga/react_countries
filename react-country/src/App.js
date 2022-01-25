9c3d66d607f571b456016f4a07af9e6161f6d608
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-01-25 17:36:42 +0100

    ✨feat(pages): creation de Home.js

--- react-country/src/App.js
+++ react-country/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <h1>Hello React</h1>
+      <Home />
     </div>
   );
 }
