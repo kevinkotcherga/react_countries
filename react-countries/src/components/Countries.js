5c355ed3e9d1c3c8707eb7a02aea865601f02708
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-01-26 14:26:04 +0100

    ✨feat(components): création de countries

--- react-countries/src/components/Countries.js
+++ react-countries/src/components/Countries.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Countries = () => {
   const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    axios.get('https://restcountries.com/v3.1/all').then((res) => setData(res.data));
+
+    console.log(data);
+
+  }, []);
+
 
   return (
     <div>
